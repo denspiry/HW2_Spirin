14251fd76b6d74362c0445deddf529fa3854a5c6
Anastasiia Sycheva <ansycheva26@gmail.com>
2025-09-15 20:14:19 +0200

    add main section with error handling for input

---
+++ calculator.py
@@ -0,0 +1,31 @@
+# def functions
+
+
+
+
+# main
+n1, operator, n2 = input().split()
+
+try:
+    n1 = float(n1)
+    n2 = float(n2)
+
+    if operator == '+':
+        res = add_fun(n1, n2) # Addition
+        
+    elif operator == '-':
+        res = subtr_fun(n1, n2) # Subtraction
+        
+    elif operator == '*':
+        res = mult_fun(n1, n2) # Multiplication
+        
+    elif operator == '/':
+        res = div_fun(n1, n2) # Division - add condition for "Division by zero!"
+    
+    else:
+        res = "Invalid arithmetic operator!"
+
+except ValueError:
+    res = "Enter the numbers!"
+
+print(res)
