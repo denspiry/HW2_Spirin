89b9473a170a39b3320ca881a5204db64919321a
denspiry <danil.spirin99@gmail.com>
2025-09-20 12:56:00 +0400

    Add div_fun with zero-division condition

--- calculator.py
+++ calculator.py
@@ -13,6 +13,12 @@ def add_fun(n1, n2):
 
 def main_fun(i):
     n1, operator, n2 = i.split()
+
+def div_fun(n1, n2):
+    if n1 == 0:
+        print('You cannot divide by Zero')
+    else:
+        return n1 / n2
     
     try:
         n1 = float(n1)
